4b62a78dc2591312a35d8944d9a179d9e2ae448c
carrasco <hugo.acasstillo@gmail.com>
2022-10-21 17:47:25 -0500

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["OnePunch"]);
+
+  const onAddCategories = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        onNewCategory={(value) => onAddCategories(value)}
+      />
+
+      {categories.map((category) => (
+        <GifGrid 
+         key={category} 
+         category={category} />
+      ))}
+    </>
+  );
+};
